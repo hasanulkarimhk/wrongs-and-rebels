a747d32fe24d95d53ac0e0e765dec12b87d3666a
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2026-03-02 23:50:49 +0600

    hghg

---
+++ wnr/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header/header.css"
+
+const Header = () => {
+  return (
+    <>
+
+    <div className="headercontainer">
+        <h4>WRONGS & REBELS &trade; </h4>
+    </div>
+
+        
+    </>
+  )
+}
+
+export default Header
